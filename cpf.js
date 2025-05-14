6055c8724e86f10b8e6889502d4d5e620eece75d
Adler Koneski <adlerkoneski@hotmail.com>
2025-05-13 21:46:35 -0300

    validação 2°DV

--- cpf.js
+++ cpf.js
@@ -22,5 +22,26 @@ function validarCPF(cpf){
     if(resto !== parseInt(cpf.substring(9, 10))){
         return false;
     }
-    
+
+    //validacao do 2° DV
+
+    soma = 0;
+
+    for(let i = 1; i <= 10; i++){
+        soma += parseInt(cpf.substring(i-1, i)) * (12 - i);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if((resto === 10) || (resto === 11)){
+        resto = 0;
+    }
+    if(resto !== parseInt(cpf.substring(10, 11))){
+        return false;
+    }
+
+
+    return true;
+
+
 }
